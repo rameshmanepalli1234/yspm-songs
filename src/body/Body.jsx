a8c2d0e69e79abe4f335ece5d506bcfb7af3e5a8
Ramesh Manepalli <ramesh.manepalli@lifesight.io>
2023-04-01 19:07:24 +0530

    Code Modified to change css of songs for mobile application

--- src/body/Body.jsx
+++ src/body/Body.jsx
@@ -1,10 +1,43 @@
 import React from "react";
+// import SimpleImageSlider from "react-simple-image-slider";
+// import image1 from '../assets/Images/sliderImages/image1.png';
+// import image2 from '../assets/Images/sliderImages/image2.jpeg';
+// import image3 from '../assets/Images/sliderImages/image3.jpeg';
+// import image4 from '../assets/Images/sliderImages/image4.jpeg';
+// import image5 from '../assets/Images/sliderImages/image5.jpeg';
+// import image6 from '../assets/Images/sliderImages/image6.jpeg';
+// import image7 from '../assets/Images/sliderImages/image7.jpeg';
 import './Body.scss';
 
+
 const Body = () => {
+    //
+    // const images = [
+    //     { url: image1 },
+    //     { url: image2 },
+    //     { url: image3 },
+    //     { url: image4 },
+    //     { url: image5 },
+    //     { url: image6 },
+    //     { url: image7 },
+    // ];
+
+
     return (
         <div className='body-div'>
-
+            {/*<div className='image-slider-container'>*/}
+            {/*<SimpleImageSlider*/}
+            {/*    width={800}*/}
+            {/*    height={504}*/}
+            {/*    images={images}*/}
+            {/*    showBullets={true}*/}
+            {/*    showNavs={true}*/}
+            {/*    autoPlay={true}*/}
+            {/*    autoPlayDelay={1}*/}
+            {/*    // useGPURender={true}*/}
+            {/*    // bgColor={'#093433'}*/}
+            {/*/>*/}
+            {/*</div>*/}
         </div>
     )
 }
